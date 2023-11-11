637759dfea4111b3978b51613d02fe445cf58879
korekso <146968075+korekso@users.noreply.github.com>
2023-11-12 00:13:14 +0100

    create initial migration

---
+++ MigrationHistory/20231111230409_InitialCreate.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Quark_Backend.MigrationHistory
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "conversations",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityAlwaysColumn)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("conversations_pkey", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "departments",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityAlwaysColumn),
+                    name = table.Column<string>(type: "character varying(30)", maxLength: 30, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("departments_pkey", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "job_positions",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityAlwaysColumn),
+                    name = table.Column<string>(type: "character varying(30)", maxLength: 30, nullable: false),
+                    department_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("job_positions_pkey", x => x.id);
+                    table.ForeignKey(
+                        name: "job_positions_department_id_fkey",
+                        column: x => x.department_id,
+                        principalTable: "departments",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityAlwaysColumn),
+                    first_name = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: true),
+                    last_name = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: true),
+                    email = table.Column<string>(type: "character varying(30)", maxLength: 30, nullable: false),
+                    password = table.Column<string>(type: "character varying(40)", maxLength: 40, nullable: true),
+                    self_description = table.Column<string>(type: "character varying(300)", maxLength: 300, nullable: true),
+                    permission_level = table.Column<int>(type: "integer", nullable: true),
+                    job_id = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("users_pkey", x => x.id);
+                    table.ForeignKey(
+                        name: "job",
+                        column: x => x.job_id,
+                        principalTable: "job_positions",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "messages",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityAlwaysColumn),
+                    sent_date = table.Column<DateOnly>(type: "date", nullable: false),
+                    text = table.Column<string>(type: "text", nullable: false),
+                    user_id = table.Column<int>(type: "integer", nullable: false),
+                    conversation_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("messages_pkey", x => x.id);
+                    table.ForeignKey(
+                        name: "messages_conversation_id_fkey",
+                        column: x => x.conversation_id,
+                        principalTable: "conversations",
+                        principalColumn: "id");
+                    table.ForeignKey(
+                        name: "messages_user_id_fkey",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users_conversations",
+                columns: table => new
+                {
+                    users_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityAlwaysColumn),
+                    conversations_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityAlwaysColumn)
+                },
+                constraints: table =>
+                {
+                    table.ForeignKey(
+                        name: "users_conversations_conversations_id_fkey",
+                        column: x => x.conversations_id,
+                        principalTable: "conversations",
+                        principalColumn: "id");
+                    table.ForeignKey(
+                        name: "users_conversations_users_id_fkey",
+                        column: x => x.users_id,
+                        principalTable: "users",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_job_positions_department_id",
+                table: "job_positions",
+                column: "department_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_messages_conversation_id",
+                table: "messages",
+                column: "conversation_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_messages_user_id",
+                table: "messages",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_job_id",
+                table: "users",
+                column: "job_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_conversations_conversations_id",
+                table: "users_conversations",
+                column: "conversations_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_conversations_users_id",
+                table: "users_conversations",
+                column: "users_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "messages");
+
+            migrationBuilder.DropTable(
+                name: "users_conversations");
+
+            migrationBuilder.DropTable(
+                name: "conversations");
+
+            migrationBuilder.DropTable(
+                name: "users");
+
+            migrationBuilder.DropTable(
+                name: "job_positions");
+
+            migrationBuilder.DropTable(
+                name: "departments");
+        }
+    }
+}

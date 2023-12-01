1db54aebc66b34594ed894210f2cf0aaa0bcc770
korekso <146968075+korekso@users.noreply.github.com>
2023-12-01 16:36:24 +0100

    Add new migration: ChangePictureUrlLength

---
+++ MigrationHistory/20231201153526_ChangePictureUrlLength.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Quark_Backend.MigrationHistory
+{
+    /// <inheritdoc />
+    public partial class ChangePictureUrlLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "picture_url",
+                table: "users",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(40)",
+                oldMaxLength: 40,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "picture_url",
+                table: "users",
+                type: "character varying(40)",
+                maxLength: 40,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+        }
+    }
+}

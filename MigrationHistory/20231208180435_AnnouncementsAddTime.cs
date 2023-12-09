83fa09edff7b916098b850dbcb85546c1ebbb3b7
kacper <106306274+kayYZ1@users.noreply.github.com>
2023-12-09 12:48:47 +0100

    add announcement method

---
+++ MigrationHistory/20231208180435_AnnouncementsAddTime.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Quark_Backend.MigrationHistory
+{
+    /// <inheritdoc />
+    public partial class AnnouncementsAddTime : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Time",
+                table: "announcements",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Time",
+                table: "announcements");
+        }
+    }
+}

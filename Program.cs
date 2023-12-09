3794768520f4851ef175601e38b6a8ea7eb91bfd
korekso <146968075+korekso@users.noreply.github.com>
2023-12-10 00:11:57 +0100

    Fix behaviour of UpdateProfile
    - returned JobPosition object no longer contains null reference of
      Department

--- Program.cs
+++ Program.cs
@@ -16,13 +16,13 @@ builder.Services.AddCors(options =>
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 builder.Services.AddDbContext<QuarkDbContext>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<ISecurityService, TokenGeneration>();
-builder.Services.AddControllers().AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
 
 builder.Services.AddAuthorization(options =>
 {

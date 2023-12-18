1b4985dcb476580cff91451ab57e657be1ff8059
korekso <146968075+korekso@users.noreply.github.com>
2023-12-18 22:30:07 +0100

    Implement GetConversation action to let client retrieve basic information about conversations that he's part of
    - change GenerateUsername method so it would generate username that would contain random number after lastNamePart

---
+++ Models/BasicConversationModel.cs
@@ -0,0 +1,22 @@
+namespace Quark_Backend.Models;
+
+
+public class BasicConversationModel
+{
+    public List<Conversation> Conversations { get; set; }
+    public class Conversation
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsPrivate { get; set; } //to let know what should be displayed on client side: name of conversation or name of user
+        public List<User> Users { get; set; }
+    }
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+    }
+}
+

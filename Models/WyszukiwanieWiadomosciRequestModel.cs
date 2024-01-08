cd756b7a86b540e7a071864cc411b91171774e48
N3xou <47154462+N3xou@users.noreply.github.com>
2024-01-08 18:04:31 +0100

    To review

--- Models/WyszukiwanieWiadomosciRequestModel.cs
+++ Models/WyszukiwanieWiadomosciRequestModel.cs
@@ -4,7 +4,7 @@
         public class WyszukiwanieWiadomosciRequest
         {
             public string? FragmentWiadomosci { get; set; }
-            public int? ConversationId { get; set; }
+            public int ConversationId { get; set; }
             public int? UserId { get; set; }
             public DateOnly? SentDate { get; set; }
         }
